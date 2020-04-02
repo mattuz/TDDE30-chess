5a4575910c07b09a279dee68fc717893ed02d798
Matthias <matge373@edu.liu.se>
2020-04-02 15:51:05 +0200

    Nu ritar den upp spelplanen på rätt sätt färmässigt också :)

--- src/schack/PieceComponent.java
+++ src/schack/PieceComponent.java
@@ -13,6 +13,7 @@ public class PieceComponent extends JComponent implements BoardListener
     public static final int CENTERTEXT = 49;
     private Board board;
     private EnumMap<PieceType, String> piece = new EnumMap<>(PieceType.class);
+    final private ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("gnome child.png"));
 
     public PieceComponent(final Board board) {
 	this.board = board;
@@ -44,16 +45,19 @@ public class PieceComponent extends JComponent implements BoardListener
 	piece.put(PieceType.ROOK, "R");
 	piece.put(PieceType.KNIGHT, "Kn");
 
-	for (int i = 0; i < board.getWidth(); i++) {
-	    for (int j = 0; j < board.getHeight(); j++) {
-		g2d.setColor(white);
-		g2d.fillRect(i * BOARDCONSTANT, j * BOARDCONSTANT,
-			     j * BOARDCONSTANT + BOARDCONSTANT, i * BOARDCONSTANT + BOARDCONSTANT);
+	for (int x = 0; x < board.getWidth(); x++) {
+	    for (int y = 0; y < board.getHeight(); y++) {
+	        if ((x%2 != 0 && y%2 == 0) || (x%2 == 0 && y%2 != 0) ) {
+	            g2d.setColor(gray);
+		} else {
+		    g2d.setColor(white);
+		}
+		g2d.fillRect(x * BOARDCONSTANT, y * BOARDCONSTANT,
+			     y * BOARDCONSTANT + BOARDCONSTANT, x * BOARDCONSTANT + BOARDCONSTANT);
 		g2d.setColor(BLACK);
-		g2d.drawRect(i * BOARDCONSTANT, j * BOARDCONSTANT, BOARDCONSTANT,
+		g2d.drawRect(x * BOARDCONSTANT, y * BOARDCONSTANT, BOARDCONSTANT,
 			     BOARDCONSTANT);
-		g2d.drawString(piece.get(board.getPieceAt(i,j)), CENTERTEXT + i * BOARDCONSTANT, CENTERTEXT + j * BOARDCONSTANT);
-
+		g2d.drawString(piece.get(board.getPieceAt(x,y)), CENTERTEXT + x * BOARDCONSTANT, CENTERTEXT + y * BOARDCONSTANT);
 	    }
 	}
     }
